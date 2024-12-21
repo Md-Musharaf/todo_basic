d8884c472b603b0fdac83a309b0f0350f04cb825
Md-Musharaf <162443742+Md-Musharaf@users.noreply.github.com>
2024-12-21 22:17:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let idNum = 1;
 
 function additem() {
-    
+
+    const input = document.querySelector('input');
+    const textBoxContent = input.value;
+    if (textBoxContent === '') {
+        alert('Please enter a todo item.');
+        return;
+    }
+
     const newDiv = document.createElement('div');
     newDiv.setAttribute("class", "divClass");
     newDiv.setAttribute("id", idNum);
 
     const span = document.createElement('span');
-    
+
     const button = document.createElement('button');
     button.setAttribute("class", "buttonClass");
-    button.setAttribute("onclick", "deleteitem("+idNum+")");
-
-    const input = document.querySelector('input');
-    const textBoxContent = input.value;
+    button.setAttribute("onclick", "deleteitem(" + idNum + ")");
 
     span.innerHTML = textBoxContent;
     button.innerHTML = "Delete";
@@ -26,7 +30,6 @@ function additem() {
 
 
 function deleteitem(idNum) {
-
     const deleteitem = document.getElementById(idNum)
     deleteitem.remove();
 }
